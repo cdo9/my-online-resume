e040d423fa7076aa53d97ac65be5500a59ba44ab
Christophe <cdo_outeiro@me.com>
2019-08-19 12:13:08 +0200

    Base style + Test header style

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,4 +1,9 @@
 @extends('layouts.main')
 @section('content')
-    Hello <i class="fad fa-cheeseburger"></i> !
+    <!--<p>Hello <i class="fad fa-cheeseburger"></i></p>
+    <div class="sign">
+        <span class="sign__word">Welcome</span>
+        <span class="sign__word"><i class="fad fa-cheeseburger"></i></span>
+
+    </div>-->
 @endsection
---
+++ resources/views/layouts/partials/header.blade.php
@@ -0,0 +1,24 @@
+<div class="header-container">
+    <header class="header">
+        <div class="header__item header__item--open-menu">
+            <i class="fad fa-ellipsis-v-alt"></i>
+        </div><!--
+        --><div class="header__item header__item--logo">
+            @cdo9
+        </div><!--
+        --><div class="header__item header__item--right">
+            <div class="header__button header__button--orange">
+                <i class="fad fa-sun"></i>
+            </div><div class="header__button header__button--cyan">
+                <i class="fad fa-palette"></i>
+            </div><div class="header__button header__button--black header__button--selected">
+                <i class="fad fa-moon"></i>
+            </div>
+        </div><!--
+        --><div class="header__item header__item--right">
+            <div class="header__button">
+                <img class="header__button__flag" src="{{ asset('images/flags/fr.png') }}"/> <i class="fad fa-caret-down"></i>
+            </div>
+        </div>
+    </header>
+</div>
