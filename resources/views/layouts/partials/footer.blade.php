1b44ed82547233e833da9ed7cd705202246c7a82
Christophe <cdo_outeiro@me.com>
2019-09-30 12:12:02 +0200

    add github/website link to footer

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -1,8 +1,8 @@
 <div class="footer">
     <div class="footer__copyright">
-        Handmade in Paris with Laravel by <a href="#" class="link">@cdo9</a>
+        Handmade in Paris with Laravel by <a href="https://cdo9.space" class="link">@cdo9</a>
     </div>
     <div class="footer__about">
-        <a href="#" class="link"><i class="fab fa-github"></i> 100% Open source project</a>
+        <a href="https://github.com/cdo9/my-online-resume" class="link"><i class="fab fa-github"></i> 100% Open source project</a>
     </div>
 </div>
