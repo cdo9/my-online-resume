e630c1feb5c758b0591515fd461bb3c03da5d1f8
Christophe <cdo_outeiro@me.com>
2019-09-30 12:13:10 +0200

    missing target="_blank"

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -3,6 +3,6 @@
         Handmade in Paris with Laravel by <a href="https://cdo9.space" class="link">@cdo9</a>
     </div>
     <div class="footer__about">
-        <a href="https://github.com/cdo9/my-online-resume" class="link"><i class="fab fa-github"></i> 100% Open source project</a>
+        <a href="https://github.com/cdo9/my-online-resume" class="link" target="_blank"><i class="fab fa-github"></i> 100% Open source project</a>
     </div>
 </div>
